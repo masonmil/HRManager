402265ce030fd8f6917ff6c4630ac6df9702fb8f
masonmil <masonis@me.com>
2017-11-29 15:03:17 -0500

    Added first test to AddressTest

--- HRTest/tests/models/address/AddressTest.cs
+++ HRTest/tests/models/address/AddressTest.cs
@@ -1,10 +1,17 @@
 ﻿using System;
+using MasonApps.HRManager.models.address;
+using Xunit;
+
 namespace HRTest.tests.models.address
 {
     public class AddressTest
     {
-        public AddressTest()
+        [Fact]
+        public void GetAddress1ReturnsAssigneddValue()
         {
+            var sut = new Address();
+            sut.Address1 = "foo";
+            Assert.Equal("foo",sut.Address1);
         }
     }
 }
