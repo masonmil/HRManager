71b329e12de8bc9e8cc6be76a2ea28a5b67d59da
masonmil <masonis@me.com>
2017-11-30 20:23:04 -0500

    Moved Test under ClassPropertyTests folder

---
+++ HRMSTest/ClassPropertyTests/TestHRManagerClassProperties.cs
@@ -0,0 +1,491 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HRMSTest
+{
+    [TestClass]
+    public class TestHRManagerClassProperties
+    {
+
+        [TestMethod]
+        public void GetAddress_Address1_EqualsAssignedValue()
+        {
+                var foo = new MasonApps.HRManager.models.address.Address();
+
+                foo.Address1 = "foo";
+                Assert.AreEqual("foo",foo.Address1);
+
+                foo.Address1 = "foo2";
+                Assert.AreEqual("foo2",foo.Address1);
+        }
+
+        [TestMethod]
+        public void GetAddress_Address2_EqualsAssignedValue()
+        {
+                var foo = new MasonApps.HRManager.models.address.Address();
+
+                foo.Address2 = "foo";
+                Assert.AreEqual("foo",foo.Address2);
+
+                foo.Address2 = "foo2";
+                Assert.AreEqual("foo2",foo.Address2);
+        }
+
+        [TestMethod]
+        public void GetAddress_City_EqualsAssignedValue()
+        {
+                var foo = new MasonApps.HRManager.models.address.Address();
+
+                foo.City = "foo";
+                Assert.AreEqual("foo",foo.City);
+
+                foo.City = "foo2";
+                Assert.AreEqual("foo2",foo.City);
+        }
+
+        [TestMethod]
+        public void GetAddress_Country_EqualsAssignedValue()
+        {
+                var foo = new MasonApps.HRManager.models.address.Address();
+
+                foo.Country = "foo";
+                Assert.AreEqual("foo",foo.Country);
+
+                foo.Country = "foo2";
+                Assert.AreEqual("foo2",foo.Country);
+        }
+
+        [TestMethod]
+        public void GetAddress_PostalCode_EqualsAssignedValue()
+        {
+                var foo = new MasonApps.HRManager.models.address.Address();
+
+                foo.PostalCode = "foo";
+                Assert.AreEqual("foo",foo.PostalCode);
+
+                foo.PostalCode = "foo2";
+                Assert.AreEqual("foo2",foo.PostalCode);
+        }
+
+        [TestMethod]
+        public void GetAddress_StateProvince_EqualsAssignedValue()
+        {
+                var foo = new MasonApps.HRManager.models.address.Address();
+
+                foo.StateProvince = "foo";
+                Assert.AreEqual("foo",foo.StateProvince);
+
+                foo.StateProvince = "foo2";
+                Assert.AreEqual("foo2",foo.StateProvince);
+        }
+
+        [TestMethod]
+        public void GetAddressInfoSample_Address1_EqualsAssignedValue()
+        {
+                var foo = new MasonApps.HRManager.models.address.sample.AddressInfoSample();
+
+                foo.Address1 = "foo";
+                Assert.AreEqual("foo",foo.Address1);
+
+                foo.Address1 = "foo2";
+                Assert.AreEqual("foo2",foo.Address1);
+        }
+
+        [TestMethod]
+        public void GetAddressInfoSample_Address2_EqualsAssignedValue()
+        {
+                var foo = new MasonApps.HRManager.models.address.sample.AddressInfoSample();
+
+                foo.Address2 = "foo";
+                Assert.AreEqual("foo",foo.Address2);
+
+                foo.Address2 = "foo2";
+                Assert.AreEqual("foo2",foo.Address2);
+        }
+
+        [TestMethod]
+        public void GetAddressInfoSample_City_EqualsAssignedValue()
+        {
+                var foo = new MasonApps.HRManager.models.address.sample.AddressInfoSample();
+
+                foo.City = "foo";
+                Assert.AreEqual("foo",foo.City);
+
+                foo.City = "foo2";
+                Assert.AreEqual("foo2",foo.City);
+        }
+
+        [TestMethod]
+        public void GetAddressInfoSample_Country_EqualsAssignedValue()
+        {
+                var foo = new MasonApps.HRManager.models.address.sample.AddressInfoSample();
+
+                foo.Country = "foo";
+                Assert.AreEqual("foo",foo.Country);
+
+                foo.Country = "foo2";
+                Assert.AreEqual("foo2",foo.Country);
+        }
+
+        [TestMethod]
+        public void GetAddressInfoSample_PostalCode_EqualsAssignedValue()
+        {
+                var foo = new MasonApps.HRManager.models.address.sample.AddressInfoSample();
+
+                foo.PostalCode = "foo";
+                Assert.AreEqual("foo",foo.PostalCode);
+
+                foo.PostalCode = "foo2";
+                Assert.AreEqual("foo2",foo.PostalCode);
+        }
+
+        [TestMethod]
+        public void GetAddressInfoSample_StateProvince_EqualsAssignedValue()
+        {
+                var foo = new MasonApps.HRManager.models.address.sample.AddressInfoSample();
+
+                foo.StateProvince = "foo";
+                Assert.AreEqual("foo",foo.StateProvince);
+
+                foo.StateProvince = "foo2";
+                Assert.AreEqual("foo2",foo.StateProvince);
+        }
+
+        [TestMethod]
+        public void GetPersonInfoAddressSample_Address1_EqualsAssignedValue()
+        {
+                var foo = new MasonApps.HRManager.models.address.sample.PersonInfoAddressSample();
+
+                foo.Address1 = "foo";
+                Assert.AreEqual("foo",foo.Address1);
+
+                foo.Address1 = "foo2";
+                Assert.AreEqual("foo2",foo.Address1);
+        }
+
+        [TestMethod]
+        public void GetPersonInfoAddressSample_Address2_EqualsAssignedValue()
+        {
+                var foo = new MasonApps.HRManager.models.address.sample.PersonInfoAddressSample();
+
+                foo.Address2 = "foo";
+                Assert.AreEqual("foo",foo.Address2);
+
+                foo.Address2 = "foo2";
+                Assert.AreEqual("foo2",foo.Address2);
+        }
+
+        [TestMethod]
+        public void GetPersonInfoAddressSample_City_EqualsAssignedValue()
+        {
+                var foo = new MasonApps.HRManager.models.address.sample.PersonInfoAddressSample();
+
+                foo.City = "foo";
+                Assert.AreEqual("foo",foo.City);
+
+                foo.City = "foo2";
+                Assert.AreEqual("foo2",foo.City);
+        }
+
+        [TestMethod]
+        public void GetPersonInfoAddressSample_Country_EqualsAssignedValue()
+        {
+                var foo = new MasonApps.HRManager.models.address.sample.PersonInfoAddressSample();
+
+                foo.Country = "foo";
+                Assert.AreEqual("foo",foo.Country);
+
+                foo.Country = "foo2";
+                Assert.AreEqual("foo2",foo.Country);
+        }
+
+        [TestMethod]
+        public void GetPersonInfoAddressSample_PostalCode_EqualsAssignedValue()
+        {
+                var foo = new MasonApps.HRManager.models.address.sample.PersonInfoAddressSample();
+
+                foo.PostalCode = "foo";
+                Assert.AreEqual("foo",foo.PostalCode);
+
+                foo.PostalCode = "foo2";
+                Assert.AreEqual("foo2",foo.PostalCode);
+        }
+
+        [TestMethod]
+        public void GetPersonInfoAddressSample_StateProvince_EqualsAssignedValue()
+        {
+                var foo = new MasonApps.HRManager.models.address.sample.PersonInfoAddressSample();
+
+                foo.StateProvince = "foo";
+                Assert.AreEqual("foo",foo.StateProvince);
+
+                foo.StateProvince = "foo2";
+                Assert.AreEqual("foo2",foo.StateProvince);
+        }
+
+        [TestMethod]
+        public void GetBusinessCard_Name_EqualsAssignedValue()
+        {
+                var foo = new MasonApps.HRManager.models.businessCard.BusinessCard();
+
+                foo.Name = "foo";
+                Assert.AreEqual("foo",foo.Name);
+
+                foo.Name = "foo2";
+                Assert.AreEqual("foo2",foo.Name);
+        }
+
+        [TestMethod]
+        public void GetBusinessCard_Notes_EqualsAssignedValue()
+        {
+                var foo = new MasonApps.HRManager.models.businessCard.BusinessCard();
+
+                foo.Notes = "foo";
+                Assert.AreEqual("foo",foo.Notes);
+
+                foo.Notes = "foo2";
+                Assert.AreEqual("foo2",foo.Notes);
+        }
+
+        [TestMethod]
+        public void GetCompany_FullName_EqualsAssignedValue()
+        {
+                var foo = new MasonApps.HRManager.models.company.Company();
+
+                foo.FullName = "foo";
+                Assert.AreEqual("foo",foo.FullName);
+
+                foo.FullName = "foo2";
+                Assert.AreEqual("foo2",foo.FullName);
+        }
+
+        [TestMethod]
+        public void GetCompany_Name_EqualsAssignedValue()
+        {
+                var foo = new MasonApps.HRManager.models.company.Company();
+
+                foo.Name = "foo";
+                Assert.AreEqual("foo",foo.Name);
+
+                foo.Name = "foo2";
+                Assert.AreEqual("foo2",foo.Name);
+        }
+
+        [TestMethod]
+        public void GetCompanyInfoSample_FullName_EqualsAssignedValue()
+        {
+                var foo = new MasonApps.HRManager.models.company.sample.CompanyInfoSample();
+
+                foo.FullName = "foo";
+                Assert.AreEqual("foo",foo.FullName);
+
+                foo.FullName = "foo2";
+                Assert.AreEqual("foo2",foo.FullName);
+        }
+
+        [TestMethod]
+        public void GetCompanyInfoSample_Name_EqualsAssignedValue()
+        {
+                var foo = new MasonApps.HRManager.models.company.sample.CompanyInfoSample();
+
+                foo.Name = "foo";
+                Assert.AreEqual("foo",foo.Name);
+
+                foo.Name = "foo2";
+                Assert.AreEqual("foo2",foo.Name);
+        }
+
+        [TestMethod]
+        public void GetContact_EmailAddress_EqualsAssignedValue()
+        {
+                var foo = new MasonApps.HRManager.models.contact.Contact();
+
+                foo.EmailAddress = "foo";
+                Assert.AreEqual("foo",foo.EmailAddress);
+
+                foo.EmailAddress = "foo2";
+                Assert.AreEqual("foo2",foo.EmailAddress);
+        }
+
+        [TestMethod]
+        public void GetContact_FirstName_EqualsAssignedValue()
+        {
+                var foo = new MasonApps.HRManager.models.contact.Contact();
+
+                foo.FirstName = "foo";
+                Assert.AreEqual("foo",foo.FirstName);
+
+                foo.FirstName = "foo2";
+                Assert.AreEqual("foo2",foo.FirstName);
+        }
+
+        [TestMethod]
+        public void GetContact_LastName_EqualsAssignedValue()
+        {
+                var foo = new MasonApps.HRManager.models.contact.Contact();
+
+                foo.LastName = "foo";
+                Assert.AreEqual("foo",foo.LastName);
+
+                foo.LastName = "foo2";
+                Assert.AreEqual("foo2",foo.LastName);
+        }
+
+        [TestMethod]
+        public void GetContact_MiddleName_EqualsAssignedValue()
+        {
+                var foo = new MasonApps.HRManager.models.contact.Contact();
+
+                foo.MiddleName = "foo";
+                Assert.AreEqual("foo",foo.MiddleName);
+
+                foo.MiddleName = "foo2";
+                Assert.AreEqual("foo2",foo.MiddleName);
+        }
+
+        [TestMethod]
+        public void GetContact_Phone_EqualsAssignedValue()
+        {
+                var foo = new MasonApps.HRManager.models.contact.Contact();
+
+                foo.Phone = "foo";
+                Assert.AreEqual("foo",foo.Phone);
+
+                foo.Phone = "foo2";
+                Assert.AreEqual("foo2",foo.Phone);
+        }
+
+        [TestMethod]
+        public void GetContactInfoSample_EmailAddress_EqualsAssignedValue()
+        {
+                var foo = new MasonApps.HRManager.models.contact.sample.ContactInfoSample();
+
+                foo.EmailAddress = "foo";
+                Assert.AreEqual("foo",foo.EmailAddress);
+
+                foo.EmailAddress = "foo2";
+                Assert.AreEqual("foo2",foo.EmailAddress);
+        }
+
+        [TestMethod]
+        public void GetContactInfoSample_FirstName_EqualsAssignedValue()
+        {
+                var foo = new MasonApps.HRManager.models.contact.sample.ContactInfoSample();
+
+                foo.FirstName = "foo";
+                Assert.AreEqual("foo",foo.FirstName);
+
+                foo.FirstName = "foo2";
+                Assert.AreEqual("foo2",foo.FirstName);
+        }
+
+        [TestMethod]
+        public void GetContactInfoSample_LastName_EqualsAssignedValue()
+        {
+                var foo = new MasonApps.HRManager.models.contact.sample.ContactInfoSample();
+
+                foo.LastName = "foo";
+                Assert.AreEqual("foo",foo.LastName);
+
+                foo.LastName = "foo2";
+                Assert.AreEqual("foo2",foo.LastName);
+        }
+
+        [TestMethod]
+        public void GetContactInfoSample_MiddleName_EqualsAssignedValue()
+        {
+                var foo = new MasonApps.HRManager.models.contact.sample.ContactInfoSample();
+
+                foo.MiddleName = "foo";
+                Assert.AreEqual("foo",foo.MiddleName);
+
+                foo.MiddleName = "foo2";
+                Assert.AreEqual("foo2",foo.MiddleName);
+        }
+
+        [TestMethod]
+        public void GetContactInfoSample_Phone_EqualsAssignedValue()
+        {
+                var foo = new MasonApps.HRManager.models.contact.sample.ContactInfoSample();
+
+                foo.Phone = "foo";
+                Assert.AreEqual("foo",foo.Phone);
+
+                foo.Phone = "foo2";
+                Assert.AreEqual("foo2",foo.Phone);
+        }
+
+        [TestMethod]
+        public void GetPerson_FirstName_EqualsAssignedValue()
+        {
+                var foo = new MasonApps.HRManager.models.person.Person();
+
+                foo.FirstName = "foo";
+                Assert.AreEqual("foo",foo.FirstName);
+
+                foo.FirstName = "foo2";
+                Assert.AreEqual("foo2",foo.FirstName);
+        }
+
+        [TestMethod]
+        public void GetPerson_LastName_EqualsAssignedValue()
+        {
+                var foo = new MasonApps.HRManager.models.person.Person();
+
+                foo.LastName = "foo";
+                Assert.AreEqual("foo",foo.LastName);
+
+                foo.LastName = "foo2";
+                Assert.AreEqual("foo2",foo.LastName);
+        }
+
+        [TestMethod]
+        public void GetPerson_MiddleName_EqualsAssignedValue()
+        {
+                var foo = new MasonApps.HRManager.models.person.Person();
+
+                foo.MiddleName = "foo";
+                Assert.AreEqual("foo",foo.MiddleName);
+
+                foo.MiddleName = "foo2";
+                Assert.AreEqual("foo2",foo.MiddleName);
+        }
+
+        [TestMethod]
+        public void GetPersonInfoSample_FirstName_EqualsAssignedValue()
+        {
+                var foo = new MasonApps.HRManager.models.person.sample.PersonInfoSample();
+
+                foo.FirstName = "foo";
+                Assert.AreEqual("foo",foo.FirstName);
+
+                foo.FirstName = "foo2";
+                Assert.AreEqual("foo2",foo.FirstName);
+        }
+
+        [TestMethod]
+        public void GetPersonInfoSample_LastName_EqualsAssignedValue()
+        {
+                var foo = new MasonApps.HRManager.models.person.sample.PersonInfoSample();
+
+                foo.LastName = "foo";
+                Assert.AreEqual("foo",foo.LastName);
+
+                foo.LastName = "foo2";
+                Assert.AreEqual("foo2",foo.LastName);
+        }
+
+        [TestMethod]
+        public void GetPersonInfoSample_MiddleName_EqualsAssignedValue()
+        {
+                var foo = new MasonApps.HRManager.models.person.sample.PersonInfoSample();
+
+                foo.MiddleName = "foo";
+                Assert.AreEqual("foo",foo.MiddleName);
+
+                foo.MiddleName = "foo2";
+                Assert.AreEqual("foo2",foo.MiddleName);
+        }
+
+
+    }
+}
